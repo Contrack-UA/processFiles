284046773a0f05359629a9e85b8c523a7dcb43d3
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-04-26 08:56:13 -0500

    Process SECOP I json

--- secop/s1.js
+++ secop/s1.js
@@ -1,43 +1,81 @@
 const jsonfile = require('jsonfile');
-const contratos = require('./../data/datos-secop2.json');
+// const contratos = require('../data/data-secop1.json');
+const file = require('../data/data-secop1.json');
+
+function encontrarSospechas(contrato) {
+  let sospechosidad = 0;
+  const sospechas = [];
+  // Valor de las adiciones superior al 10% del valor del contrato
+  if (contrato[47] > contrato[46] * 0.35) {
+    sospechosidad += 1;
+    sospechas.push('Anticipo muy alto');
+  }
+  // Estado 'Terminado anormalmente después de convocado'
+  if (contrato[12] === 'Terminado Anormalmente después de Convocado') {
+    sospechosidad += 1;
+    sospechas.push('Estado anormal');
+  }
+  // Plazo de ejecución no definido
+  if (contrato[41] === 'No definido') {
+    sospechosidad += 1;
+    sospechas.push('Plazo de ejecución no definido');
+  } else if (contrato[41] === 'D') {
+    // Tiempo de adición superior a la mitad del plazo de ejecución del proyecto
+    if (contrato[42] >= contrato[40] * 0.5) {
+      sospechosidad += 1;
+      sospechas.push('Mucho tiempo de adición');
+    }
+  } else if (contrato[41] === 'M') {
+    // Tiempo de adición superior a la mitad del plazo de ejecución del proyecto
+    if (contrato[43] >= contrato[40] * 0.5) {
+      sospechosidad += 1;
+      sospechas.push('Mucho tiempo de adición');
+    }
+  }
+  // 8 o más campos sin definir
+  let count = 0;
+  for (let i = 0; i < contrato.length; i += 1) {
+    const campo = contrato[i];
+    if (!campo || campo === 0 || campo === 'No definido') {
+      count += 1;
+    }
+  }
+  if (count >= 8) {
+    // console.log('hay');
+    sospechosidad += 1;
+    sospechas.push('8 o más campos sin definir');
+  }
+  return {
+    sospechosdidad: sospechosidad,
+    sospechas: sospechas
+  };
+}
+
 
 module.exports.process = function process() {
+  const contratos = file.data;
   const newFile = [];
   let pagina = 0;
   for (let i = 0; i < contratos.length; i += 1) {
+    const contrato = contratos[i];
+    // console.log(contrato[12]);
     if (i % 6 === 0) {
       pagina += 1;
     }
-    let sospechosidad = 0;
-    const sospechas = [];
-    const contrato = contratos[i];
-    if (contrato['Estado Contrato'] === 'Modificado') {
-      sospechosidad += 1;
-      sospechas.push('Se ha modificado el estado');
-    }
-    const precioBase = contrato['Precio Base Propuesta'];
-    precioBase.replace(/,/g, '').replace(/.00/g, '');
-    if (precioBase === '-') {
-      sospechosidad += 1;
-      sospechas.push('Precio base 0');
-    }
-    const anticipo = contrato['Porcentaje Anticipo'];
-    if (anticipo !== '-' && anticipo > 15) {
-      sospechosidad += 1;
-      sospechas.push('Anticipo alto');
-    }
-    const valorFacturado = contrato['Valor Facturado'];
-    valorFacturado.replace(/,/g, '').replace(/.00/g, '');
-    if (valorFacturado !== '-' && valorFacturado > precioBase) {
-      sospechosidad += 1;
-      sospechas.push('Facturas mayores a valor base');
-    }
-    contrato.sospechosidad = sospechosidad;
-    contrato.sospechas = sospechas;
-    contrato.pagina = pagina;
-    newFile.push(contrato);
+    let contratoObj = {};
+    contratoObj._id = contrato[0];
+    contratoObj.nomEntidad = contrato[10];
+    contratoObj.detalle = contrato[16];
+    contratoObj.lugar = contrato[19];
+    contratoObj.valor = contrato[46];
+    contratoObj.pagina = pagina;
+    const sos = encontrarSospechas(contrato);
+    contratoObj.sospechosidad = sos.sospechosidad;
+    contratoObj.sospechas = sos.sospechas;
+    console.log(contratoObj.sospechas);
+    newFile.push(contratoObj);
   }
-  jsonfile.writeFile('../results/secop2.json', newFile, (err) => {
+  jsonfile.writeFile('secop1.json', newFile, (err) => {
     console.error(err);
   });
 };
--- secop/s2.js
+++ secop/s2.js
@@ -1,35 +1,35 @@
-var jsonfile = require('jsonfile');
-var contratos = require('../data/datos-secop2.json');
+const jsonfile = require('jsonfile');
+const contratos = require('../data/datos-secop2.json');
 
-module.exports.process = function() {
-  var newFile = [];
-  var pagina = 0;
-  for(var i=0; i<contratos.length; i++) {
-    if(i%6 === 0) {
-      pagina++;
+module.exports.process = function process() {
+  const newFile = [];
+  let pagina = 0;
+  for (let i = 0; i < contratos.length; i += 1) {
+    if (i % 6 === 0) {
+      pagina += 1;
     }
-    var sospechosidad = 0;
-    var sospechas = [];
-    var contrato = contratos[i];
-    if(contrato['Estado Contrato'] === 'Modificado') {
-      sospechosidad++;
+    let sospechosidad = 0;
+    const sospechas = [];
+    const contrato = contratos[i];
+    if (contrato['Estado Contrato'] === 'Modificado') {
+      sospechosidad += 1;
       sospechas.push('Se ha modificado el estado');
     }
-    var precioBase = contrato['Precio Base Propuesta'];
+    const precioBase = contrato['Precio Base Propuesta'];
     precioBase.replace(/,/g, '').replace(/.00/g, '');
-    if(precioBase === '-') {
-      sospechosidad++;
+    if (precioBase === '-') {
+      sospechosidad += 1;
       sospechas.push('Precio base 0');
     }
-    var anticipo = contrato['Porcentaje Anticipo'];
-    if(anticipo !== '-' && anticipo > 15) {
-      sospechosidad++;
+    const anticipo = contrato['Porcentaje Anticipo'];
+    if (anticipo !== '-' && anticipo > 15) {
+      sospechosidad += 1;
       sospechas.push('Anticipo alto');
     }
-    var valorFacturado = contrato['Valor Facturado'];
+    const valorFacturado = contrato['Valor Facturado'];
     valorFacturado.replace(/,/g, '').replace(/.00/g, '');
-    if(valorFacturado != '-' && valorFacturado > precioBase) {
-      sospechosidad++;
+    if (valorFacturado !== '-' && valorFacturado > precioBase) {
+      sospechosidad += 1;
       sospechas.push('Facturas mayores a valor base');
     }
     contrato.sospechosidad = sospechosidad;
@@ -37,7 +37,7 @@ module.exports.process = function() {
     contrato.pagina = pagina;
     newFile.push(contrato);
   }
-  jsonfile.writeFile('../results/secop2.json', newFile, function(err) {
-    console.error(err)
+  jsonfile.writeFile('../results/secop2.json', newFile, (err) => {
+    console.error(err);
   });
-}
+};
